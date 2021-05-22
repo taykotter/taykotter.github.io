fa6d5d91d42f16be0ad97ee1937d1a3a8a2afa0c
taykotter <sul17003@byui.edu>
2021-05-22 09:42:52 -0600

    Week 5 Preston Page

--- WWD230/week4/lesson4/js/preston-4.js
+++ WWD230/week4/lesson4/js/preston-4.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
 }
+
+var date = new Date();
+var nummonth = date.getMonth();
+const month = date.toLocaleString('default', { month: 'long' });
+var day = date.getDay();
+var weekday = date.toLocaleString("default", { weekday: "long" })
+var year = date.getFullYear();
+var todayDate = weekday + ', ' + day + ' ' + month + ' '+ year;
+
+document.getElementById("todayDate").innerHTML = todayDate;
