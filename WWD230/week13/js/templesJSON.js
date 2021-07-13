181dcdc355ef8db80179a169cfa1839c703bfb73
taykotter <sul17003@byui.edu>
2021-07-13 17:25:15 -0600

    Week 13 Code

--- WWD230/week13/js/templesJSON.js
+++ WWD230/week13/js/templesJSON.js
@@ -6,25 +6,22 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    // temporary checking for valid response and data parsing
+
+    const temples = jsonObject['temples'];
+
+    for (let i = 0; i < temples.length; i++) {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let address = document.createElement('span');
+      let city = document.createElement('span');
 
     
-    const events = jsonObject.towns[6].events;
+    
+    
+    }
 
-        let card = document.createElement('section');
-        let event0 = document.createElement('p');
-        let event1 = document.createElement('p');
-        let event2 = document.createElement('p');
-
-
-
-        event0.textContent = events[0];
-        event1.textContent = events[1];
-        event2.textContent = events[2];
-
-        card.appendChild(event0);
-        card.appendChild(event1);
-        card.appendChild(event2);
+        
 
         document.querySelector('div.event').appendChild(card);
 });
