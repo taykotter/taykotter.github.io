98cf6fd057fca4075c8fdd432c58d16e5dc56d02
taykotter <sul17003@byui.edu>
2021-07-13 17:44:51 -0600

    Week 12 Christmas Ranch Tutorial: Practicing SCSS

--- WWD230/week13/js/templesJSON.js
+++ WWD230/week13/js/templesJSON.js
@@ -13,22 +13,29 @@ fetch(requestURL)
     for (let i = 0; i < temples.length; i++) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let address = document.createElement('span');
-      let city = document.createElement('span');
-      let state = document.createElement('span');
-      let zip = document.createElement('span');
+      let address = document.createElement('p');
+      let number = document.createElement('p');
+      let picture = document.createElement('img');
+
+
+      h2.textContent = temples[i].name;
+      h2.address = temples[i].address + " " + temples[i].city + " " + temples[i].state;
       picture.setAttribute('src', temples[i].imageurl);
-      picture.setAttribute('alt', temple[i].name);
+      picture.setAttribute('alt', temples[i].name);
 
-      
 
+
+      card.appendChild(h2);
+      card.appendChild(address);
+      card.appendChild(number);
+      card.appendChild(picture);
 
     
     
-    
+      document.querySelector('div.temples').appendChild(card);
     }
 
         
 
-        document.querySelector('div.event').appendChild(card);
+      
 });
